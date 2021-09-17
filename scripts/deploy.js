447da307b5f95c72da75fa795e7c94f1382534fe
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-09-17 18:42:50 +0800

    Vite React init

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Account balance: `, (await deployer.getBalance()).toString());
+
+  const Token = await hre.ethers.getContractFactory("WavePortal");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  console.log(`WavePortal address ${token.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
