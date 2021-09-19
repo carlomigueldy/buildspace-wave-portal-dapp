203de7b8269bed13bf47569b7d2e47ee5d3372d3
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-09-19 11:34:52 +0800

    Add realtime listener on new wave created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ import moment from "moment";
 
 const WAVE_PORTAL_ADDRESS = import.meta.env.PROD
   ? TESTNET_WAVE_PORTAL_ADDRESS
-  : "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  : "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
 function useWavePortalContract() {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -100,6 +100,28 @@ export default function Home() {
     checkIfWalletIsConnected();
     getWaves();
     // getTotalWaves();
+
+    contract?.on(
+      "WaveCreated",
+      (index, owner, display_name, message, created_at) => {
+        const payload = {
+          index,
+          owner,
+          display_name,
+          message,
+          created_at,
+        };
+        console.log("WaveCreated", {
+          payload,
+        });
+
+        setState({
+          ...state,
+          totalWaves: state.waves.length + 1,
+          waves: [...state.waves, payload],
+        });
+      }
+    );
   }, []);
 
   async function showNoWalletFoundToast() {
@@ -177,10 +199,15 @@ export default function Home() {
 
     await connectWallet();
 
-    const waveTxn = await contract?.wave({
-      display_name: form.displayName,
-      message: form.message,
-    });
+    const waveTxn = await contract?.wave(
+      {
+        display_name: form.displayName,
+        message: form.message,
+      },
+      {
+        gasLimit: 300_000,
+      }
+    );
     setTransactionLoading(true);
     console.log("⛏️ Mining...", waveTxn.hash);
     await waveTxn.wait();
@@ -188,7 +215,7 @@ export default function Home() {
     setTransactionLoading(false);
     setForm(formDefaultValue);
     closeFormModal();
-    getWaves();
+    // getWaves();
     setShowConfetti(true);
     openSuccessModal();
 
